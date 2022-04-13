47f9fb7cfd188ee419108b44ba04916edbca17dc
pcTop <stephenhavsen2002@gmail.com>
2022-04-13 15:40:16 +0430

    initial commit

---
+++ src/Navigator.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * this class navigate us to the target classes
+ */
+public class Navigator {
+
+
+    public  static  void main(String []args){
+        System.out.println("WE finally made the Navigator");
+    }
+}
